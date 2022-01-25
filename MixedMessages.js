b1b3b53e9f86cd2ca1ded94884d15f867bed32c9
Brennan Emerson <brennanemerson15@gmail.com>
2022-01-24 18:40:40 -0800

    'modified the comments on the code

--- MixedMessages.js
+++ MixedMessages.js
@@ -3,12 +3,13 @@ let adjective = ["Short","chubby","lazy", "Stumpy"]
 let noun = ["Girl","Cat","Corgi"];
 let verb = ["Jump", "Run", "Fetch"];
 
-//Function that displays different jokes everytime it's run
+//Function that displays different jokes everytime it's run by taking a adjective, noun and verb from 3 different arrays.
 let mixedMessages = (adj, noun, verb) => {
+    //thsese variables take in a specific parameter which hols an array, and then stores a random index number.
    const randomAdjective = Math.floor(Math.random() * adj.length)
    const randomNoun = Math.floor(Math.random() * noun.length)
    const randomVerb = Math.floor(Math.random() * verb.length)
-
+    
    console.log(`The ${noun[randomNoun]} is trying to ${verb[randomVerb]} but it's too ${adj[randomAdjective]} to do so.`)
 };
 
